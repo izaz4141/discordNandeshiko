a22434911baea8812daf0f846bfd15d783e1113d
izaz4141 <46811608+izaz4141@users.noreply.github.com>
2022-02-10 18:13:43 +0700

    trying pytorch

--- launcher.py
+++ launcher.py
@@ -1,6 +1,9 @@
 from os import system
+from nltk import download
 
+download('punkt')
 system("python -m pip install -U git+https://github.com/Rapptz/discord-ext-menus")
+system("python -m pip install torch torchvision torchaudio")
 
 from features.bot_main import bot
 

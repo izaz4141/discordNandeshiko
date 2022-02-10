e061e1b5b0224abd57769a008f38ae4a237a820e
izaz4141 <46811608+izaz4141@users.noreply.github.com>
2022-02-10 18:22:16 +0700

    trying chatterbot

--- launcher.py
+++ launcher.py
@@ -1,9 +1,8 @@
 from os import system
-from nltk import download
+# from nltk import download
 
-download('punkt')
+# download('punkt')
 system("python -m pip install -U git+https://github.com/Rapptz/discord-ext-menus")
-system("python -m pip install torch torchvision torchaudio")
 
 from features.bot_main import bot
 

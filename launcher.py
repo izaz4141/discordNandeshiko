913543b23eb36bd479f22487d8b111e8222ae8a7
izaz4141 <46811608+izaz4141@users.noreply.github.com>
2022-02-10 19:50:02 +0700

    tring chatterbot spacy

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ from os import system
 # from nltk import download
 
 # download('punkt')
-system("python -m pip install -U git+https://github.com/Rapptz/discord-ext-menus")
+# system("python -m pip install -U git+https://github.com/Rapptz/discord-ext-menus")
 
 from features.bot_main import bot
 

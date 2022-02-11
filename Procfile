a7880316e0ebcdcb8885cab43bfac37af1b28ff7
izaz4141 <46811608+izaz4141@users.noreply.github.com>
2022-02-11 18:26:44 +0700

    linking spacy en model

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: python -m spacy link en_core_web_md en
 worker: python launcher.py
--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ from os import system
 # from nltk import download
 
 # download('punkt')
-# system("python -m pip install -U git+https://github.com/Rapptz/discord-ext-menus")
+system("python -m spacy link en_core_web_md en")
 
 from features.bot_main import bot
 

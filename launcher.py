4cf4e40cd09c2b89341fdadfbde90899d2da18e8
izaz4141 <46811608+izaz4141@users.noreply.github.com>
2022-02-11 18:45:05 +0700

    spacy en download

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ from os import system
 # from nltk import download
 
 # download('punkt')
-system("sudo python -m spacy download en")
+system("python -m spacy download en")
 
 from features.bot_main import bot
 

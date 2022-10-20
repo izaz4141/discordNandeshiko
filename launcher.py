929763b6402cdb0a7cd21c3a9f3ed94524cb8ffb
Glicole <widyanizaz@gmail.com>
2022-10-20 08:20:46 +0700

    change test to file

--- launcher.py
+++ launcher.py
@@ -4,8 +4,5 @@ VERSION = "0.0.3"
 def main():
     bot.run(VERSION)
 
-def test_main():
-    bot.run(VERSION)
-
 if __name__ == '__main__':
     main()
---
+++ test_launcher.py
@@ -0,0 +1,6 @@
+from features.bot_main import bot
+
+VERSION = "0.0.3"
+
+def test_main():
+    bot.run(VERSION)

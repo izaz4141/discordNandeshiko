dd71be0d67476b566dd4f83c1843bdbea3da9849
Glicole <widyanizaz@gmail.com>
2022-10-20 08:17:31 +0700

    added test func

--- launcher.py
+++ launcher.py
@@ -1,13 +1,11 @@
-# from os import system
-# system('python -m pip uninstall discord.py&&y')
-# system('python -m pip install saucenao-api==2.3.1')
-
-
 from features.bot_main import bot
 
 VERSION = "0.0.3"
 def main():
-    bot.run(VERSION);
+    bot.run(VERSION)
+
+def test_main():
+    bot.run(VERSION)
 
 if __name__ == '__main__':
-    main();
+    main()

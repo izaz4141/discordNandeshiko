7b6a6085142ce4fc5ab8cee78d7219e616b1b50a
izaz4141 <izazwidyan@gmail.com>
2023-09-25 23:25:44 +0700

    added genshin artifact and profile

--- features/cogs/mihoyo.py
+++ features/cogs/mihoyo.py
@@ -7,18 +7,24 @@ import asyncio
 
 from mihomo import Language, MihomoAPI
 from mihomo.models import StarrailInfoParsed
+from enkanetwork import EnkaNetworkAPI
 
 from ..utils import hsr
 from ..db import db
 
 
-client = MihomoAPI(language=Language.EN)
+hsr_client = MihomoAPI(language=Language.EN)
+gi_client = EnkaNetworkAPI()
 OPTIONS = {
     "1️⃣": 0,
     "2⃣": 1,
     "3⃣": 2,
     "4⃣": 3,
     "5⃣": 4,
+    "6️⃣": 5,
+    "7️⃣": 6,
+    "8️⃣": 7,
+    "9️⃣": 8,
 }
 
 class MiHoYo(Cog):
@@ -46,7 +52,7 @@ class MiHoYo(Cog):
                 return await ctx.send("Mmmm... maaf, Nadeshiko tidak bisa menemukan UID HSR kakak~\nCoba command register_hsr untuk mendaftarkan UID-nya!")
         
         
-        data: StarrailInfoParsed = await client.fetch_user(uid, replace_icon_name_with_url=True)
+        data: StarrailInfoParsed = await hsr_client.fetch_user(uid, replace_icon_name_with_url=True)
         choice = Embed(
             title= "Select Characters",
             description= "\n".join([f"{i}. {chara.name}" for i, chara in enumerate(data.characters)]),
@@ -91,6 +97,100 @@ class MiHoYo(Cog):
     async def register_hsr_slash(self, ctx, *, uid):
         db.execute("UPDATE exp SET HSR_UID = ? WHERE UserID = ?", uid, ctx.author.id)
         await ctx.respond("Okeee~ lain kali kalau ngecek akun HSR tidak usah pake UID lagi!")
+
+    @command(name="register_gi")
+    async def register_gi(self, ctx, *, uid):
+        """Menyimpan UID GI-mu
+
+        Args:
+            uid (int): UID GI
+        """
+        if uid.isnumeric():
+            uid = int(uid)
+        else:
+            return await ctx.send("Coba lagi kak (˶ᵔ ᵕ ᵔ˶)")
+        
+        db.execute("UPDATE exp SET GI_UID = ? WHERE UserID = ?", uid, ctx.author.id)
+        await ctx.send("Okeee~ lain kali kalau ngecek akun GI tidak usah pake UID lagi!")
+        return
+    @slash_command(name="register_gi", description="Menyimpan UID GI-mu")
+    @option("uid", int, description="UID GI")
+    async def register_gi_slash(self, ctx, *, uid):
+        db.execute("UPDATE exp SET GI_UID = ? WHERE UserID = ?", uid, ctx.author.id)
+        await ctx.respond("Okeee~ lain kali kalau ngecek akun GI tidak usah pake UID lagi!")
+
+    @command(name="artifact_eval", aliases=["ae"])
+    async def artifact_eval(self, ctx, *, uid: Optional[str]="0"):
+        """Melihat Artifact pada karakter di Genshin Impact
+
+        Args:
+            uid (Optional[int]): UID GI
+        """
+        def _check(r, u):
+            return (
+                r.emoji in OPTIONS.keys()
+                and u == ctx.author
+                and r.message.id == msg.id
+            )
+        uid = int(uid)
+
+        if uid == 0:
+            uid = db.record("SELECT GI_UID FROM exp WHERE UserID = ?", ctx.author.id)[0]
+            if uid == 0:
+                return await ctx.send("Mmmm... maaf, Nadeshiko tidak bisa menemukan UID GI kakak~\nCoba command register_gi untuk mendaftarkan UID-nya!")
+        
+        
+        data = await gi_client.fetch_user(uid)
+        choice = Embed(
+            title= "Select Characters",
+            description= "\n".join([f"{i}. {chara.name}" for i, chara in enumerate(data.characters)]),
+            colour= ctx.author.colour,
+            timestamp = datetime.utcnow()
+        )
+        msg = await ctx.send(embed=choice)
+        for emoji in list(OPTIONS.keys())[:min(len(data.characters), len(OPTIONS))]:
+            await msg.add_reaction(emoji)
+        try:
+            reaction, _ = await self.bot.wait_for("reaction_add", timeout=25.0, check=_check)
+        except asyncio.TimeoutError:
+            await msg.delete()
+            return None
+        else:
+            await msg.delete()
+            chara = data.characters[OPTIONS[reaction.emoji]]
+            artifacts = hsr.artifact_eval(chara, uid)
+            await ctx.send(embed=artifacts)
+    @slash_command(name="artifact_eval", description="Melihat Relic pada karakter di Genshin Impact")
+    @option("uid", int, description="UID GI", default=None)
+    async def artifact_eval_slash(self, ctx, uid):
+        await self.artifact_eval(ctx, uid=uid)
+
+    @command(name="gi_profile", aliases=["gp"])
+    async def gi_profile(self, ctx, *, uid: Optional[str]="0"):
+        uid = int(uid)
+
+        if uid == 0:
+            uid = db.record("SELECT GI_UID FROM exp WHERE UserID = ?", ctx.author.id)[0]
+            if uid == 0:
+                return await ctx.send("Mmmm... maaf, Nadeshiko tidak bisa menemukan UID GI kakak~\nCoba command register_gi untuk mendaftarkan UID-nya!")
+        
+        data = await gi_client.fetch_user(uid)
+        embed = Embed(
+            title= f"{data.player.nickname}         ({data.player.level})",
+            description= f"*{data.player.signature}*",
+            color=ctx.author.colour
+        )
+        fields = [
+            ("Abyss Floor", f"{data.player.abyss_floor} - {data.player.abyss_room}")
+        ]
+        for name, value in fields:
+            if value == '':
+                    value = "N/A"
+            embed.add_field(name=name, value=value)
+        embed.set_thumbnail(url= data.player.avatar.icon.url)
+        embed.set_footer(text= f"UID: {uid}")
+
+        await ctx.send(embed=embed)
 
     @Cog.listener()
     async def on_ready(self):
--- features/utils/hsr.py
+++ features/utils/hsr.py
@@ -26,3 +26,32 @@ def relic_scorer(chara, uid):
     relics.set_thumbnail(url= chara.icon)
     relics.set_footer(text= f"UID: {uid}")
     return relics
+
+def artifact_eval(chara, uid):
+    if chara.rarity == 5:
+        color = Colour.gold()
+    else:
+        color = Colour.purple()
+    relics = Embed(
+        title= chara.name,
+        colour= color
+    )
+    fields = []
+    for eq in chara.equipments:
+        if eq.props == []:
+            continue
+        enhance = {}
+        for prop in eq.props:
+            try:
+                enhance[prop.prop_id] += 1
+            except KeyError:
+                enhance[prop.prop_id] = 1
+        subfix = '\n'.join([f"{substat.name} +{enhance[substat.prop_id]}:  {substat.value}{'%' if substat.type == 1 else ''}" for substat in eq.detail.substats])
+        fields += [(f"{eq.detail.name} +{eq.level}", f"**{eq.detail.mainstats.name}:  {eq.detail.mainstats.value}{'%' if eq.detail.mainstats.type == 1 else ''}**\n{subfix}", True)]
+    for name, value, inline in fields:
+        if value == '':
+                value = "N/A"
+        relics.add_field(name=name, value=value, inline=inline)
+    relics.set_thumbnail(url= chara.image.card.url)
+    relics.set_footer(text= f"UID: {uid}")
+    return relics
